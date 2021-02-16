9f15715cefdc75bd42c5961b3f31315459537661
Konstantsin Yurkevich <kutcevolov@iclod.com>
2021-02-16 17:42:14 +0300

    add task2

---
+++ Java/Fundamental/MainTask/task2.java
@@ -0,0 +1,9 @@
+
+
+public class task2 {
+    public static void main(String[] args) {
+        for (int i = args.length - 1; i < args.length && i >= 0; i--) {
+            System.out.println(args[i]);
+        }
+    }
+}
