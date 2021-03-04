60156a44d40259a6a418a867e8b27969f69cc5aa
Konstantsin <74815757+KonstantsinYurkevich@users.noreply.github.com>
2021-03-04 20:21:40 +0300

    update task3.java

--- Java/Fundamental/MainTask/task3.java
+++ Java/Fundamental/MainTask/task3.java
@@ -3,14 +3,16 @@ public class task3 {
     public static void main(String[] args)  {
         System.out.println("enter count of numbers");
         Scanner console = new Scanner(System.in);
-        String name = console.next();
-        int n = Integer.parseInt(name);
-        for (int i = 0; i < n; i+=1) {
-            int a = (int) (Math.random() * (10000+ 1));
-            System.out.println(a);
-        }for (int i = 0; i < n; i+=1) {
-            int b = (int) (Math.random() * (10000 + 1));
-            System.out.print(b+" ");
+        int count = console.nextInt();
+        Random rand = new Random();
+        int [] arr = new int[count];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] =  rand.nextInt(100);
+            System.out.println(arr[i] + " ");
+        }
+        System.out.println();
+        for ( int i = 0; i < arr.length; i ++){
+            System.out.print( arr [i] + " ");
         }
     }
 }
